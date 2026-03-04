170ba6fab87fc9b9f17aaec12206405394e88f7c
juhairulislam <juhairulislam2018@gmail.com>
2026-03-04 07:29:09 +0600

    feat: showing empty masssage and fix showing null

--- script/index.js
+++ script/index.js
@@ -31,6 +31,30 @@ const displayLevelWord = (words) =>{
     
     const wordContainer = document.getElementById("word-container"); 
     wordContainer.innerHTML = ' ' ;
+
+
+    // if lesson contain no word then 
+
+    if(words.length === 0){
+
+            wordContainer.innerHTML = `
+
+             <div class="text-center col-span-full rounded-xl py-10 space-y-6">
+
+              <img class="mx-auto" src="./assets/alert-error.png" alt="">
+
+        <p class=" font-normal text-gray-400 font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-semibold text-3xl font-bangla">নেক্সট Lesson এ যান</h2>
+       </div>
+            
+            ` ;
+
+            
+            return;
+
+
+
+    }
     
     
     // step 2
@@ -42,10 +66,10 @@ const displayLevelWord = (words) =>{
     const card  = document.createElement('div') ;
     card.innerHTML = `
      <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-xl">${word.word}</h2>
+            <h2 class="font-bold text-xl">${word.word ? word.word : 'শব্দ পাওয়া যায়নি'}</h2>
             <p class="font-semibold">Meaning /Pronounciation</p>
             
-            <div class="font-medium text-2xl font-bangla">"${word.meaning}/ ${word.pronunciation}"</div>
+            <div class="font-medium text-2xl font-bangla">"${word.meaning? word.meaning: 'অর্থ পাওয়া যায়নি'}/ ${word.pronunciation?word.pronunciation: 'Pronunciation পাওয়া যায়নি'}"</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button> 
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-volume-high"></i></button>
